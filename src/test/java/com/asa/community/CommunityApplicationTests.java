bcbbb29450e9f6408c7f87919326f7a7e61e5427
AsaZws <zhouws.com@gmail.com>
2020-01-16 12:01:39 +0800

    git init

---
+++ src/test/java/com/asa/community/CommunityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.asa.community;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommunityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
